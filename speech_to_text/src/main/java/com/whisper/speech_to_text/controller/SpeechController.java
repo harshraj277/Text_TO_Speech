83d23e949349ea86f94947bc78686191ea647c98
harshraj277 <harshrajp277@gmail.com>
2026-03-20 22:53:06 +0530

    changes  made

--- speech_to_text/src/main/java/com/whisper/speech_to_text/controller/SpeechController.java
+++ speech_to_text/src/main/java/com/whisper/speech_to_text/controller/SpeechController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin
+@CrossOrigin("*")
 public class SpeechController {
 
     @PostMapping("/speech")
